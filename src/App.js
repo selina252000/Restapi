2c9a9b163ee8938a013fff36a3a3f1d47d19e60c
Selina252000 <selina.darnal@netsmartz.net>
2022-09-16 12:33:22 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [posts,setPosts] = useState([]);
+
+useEffect(() =>{
+  axios.get("https://jsonplaceholder.typicode.com/posts")
+  .then((response)=>{
+   setPosts(response.data);
+  });
+},[]);
+ 
+console.log("DATA >>", posts)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <ul>
+     {posts?.map((post) => (
+        <li>{post.title}</li>
+     ))
+     
+     }
+     </ul>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+
---
+++ src/Person.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const  Person  = (props)  =>{
+    return <>
+   
+     <h1> Hi My Name is {props.name}</h1>
+     <p>Person file</p>
+   
+    </>
+   }
+   export default  Person
